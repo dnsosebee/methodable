ed327dd671c739a414bfc9e126f679d178f1cd3f
Daniel Sosebee <dnsosebee@Daniels-MacBook-Pro.local>
2022-04-13 09:20:06 -0600

    weird propagation select logic

--- components/block/Block.tsx
+++ components/block/Block.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import { EditorContent, NodeViewWrapper, useEditor } from "@tiptap/react";
 import Document from "@tiptap/extension-document";
 import Paragraph from "@tiptap/extension-paragraph";
@@ -7,14 +7,17 @@ import History from "@tiptap/extension-history";
 
 interface BlockProps {
   tempNum: number;
-  selectionStartCallback?: () => void;
+  selectionStartCallback?: (isSelectedBlock: boolean) => void;
   selectionChangeCallback?: () => void;
   selectionEndCallback?: () => void;
 }
 
 const Block = (props: BlockProps) => {
   const subrender = Math.random() > 0.7;
-
+  let clickOriginatedHere = useRef(false); // whether the current click started in this component
+  let isAncestorOfSelectionStart = useRef(false);
+  let isParentOfSelectionStart = useRef(false);
+  let isParentOfEntireSelection = useRef(false);
   const isMouseDown = (e: React.MouseEvent): boolean => {
     return (e.buttons & 1) === 1;
   };
@@ -26,7 +29,9 @@ const Block = (props: BlockProps) => {
 
   const mouseEnter = (e: React.MouseEvent) => {
     if (isMouseDown(e)) {
+      props.selectionChangeCallback();
       console.log("onMouseEnter mouseIsDown " + props.tempNum);
+
     } else {
       console.log("onMouseEnter mouseisUp " + props.tempNum);
     }
@@ -34,17 +39,23 @@ const Block = (props: BlockProps) => {
 
   const mouseLeave = (e: React.MouseEvent) => {
     if (isMouseDown(e)) {
+      if (clickOriginatedHere.current) {
+        props.selectionStartCallback(true);
+      }
       console.log("onMouseLeave mouseIsDown " + props.tempNum);
     } else {
       console.log("onMouseLeave mouseisUp " + props.tempNum);
     }
+    clickOriginatedHere.current = false;
   };
 
   const mouseDown = () => {
+    clickOriginatedHere.current = true;
     console.log("onMouseDown " + props.tempNum);
   };
 
   const mouseUp = () => {
+    clickOriginatedHere.current = false;
     console.log("onMouseUp " + props.tempNum);
   };
 
@@ -52,12 +63,32 @@ const Block = (props: BlockProps) => {
     console.log("onCopy " + props.tempNum);
   };
 
+  // isSelectedBlock: whether the callback is coming directly from the block that was selected
+  const selectionStartFromChild = (isSelectedBlock: boolean) => {
+    if (isSelectedBlock) {
+      isParentOfSelectionStart.current = true;
+    }
+    isAncestorOfSelectionStart.current = true;
+    props.selectionStartCallback(false);
+    console.log("Selection started")
+  }
+
+  const selectionChangeFromChild = () => {
+    if (isAncestorOfSelectionStart.current) {
+      isParentOfEntireSelection.current = true;
+      console.log("The parent of the selection is: " + props.tempNum)
+    } else {
+      props.selectionChangeCallback();
+    }
+    console.log("Selection changed")
+  }
+
   return (
     <div className="bg-red-300">
       <div className="flex">
         <div className="h-6 w-6 bg-yellow-300 border border-black"></div>
         <p
-          className="select-none"
+          className="select-none flex-grow"
           onClick={click}
           onMouseEnter={mouseEnter}
           onMouseLeave={mouseLeave}
@@ -72,9 +103,9 @@ const Block = (props: BlockProps) => {
         <div className="flex">
           <div className="bg-blue-300 w-6 border border-black"></div>
           <div>
-            <Block tempNum={props.tempNum + 1} />
-            <Block tempNum={props.tempNum + 1.1} />
-            <Block tempNum={props.tempNum + 1.01} />
+            <Block selectionStartCallback={selectionStartFromChild} selectionChangeCallback={selectionChangeFromChild} tempNum={props.tempNum + 1} />
+            <Block selectionStartCallback={selectionStartFromChild} selectionChangeCallback={selectionChangeFromChild} tempNum={props.tempNum + 1.1} />
+            <Block selectionStartCallback={selectionStartFromChild} selectionChangeCallback={selectionChangeFromChild} tempNum={props.tempNum + 1.01} />
           </div>
         </div>
       )}
