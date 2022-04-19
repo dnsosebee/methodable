c90930b850a5308732b919296d5b88947a0ddc67
dnsosebee <dnsosebee@gmail.com>
2022-04-18 19:23:27 -0600

    enable multiple renderings of one block by decoupling block info from component hierarchy

--- client/model/state/stateTypes.ts
+++ client/model/state/stateTypes.ts
@@ -1,16 +1,26 @@
+
 export interface IState {
-  rootBlock: IBlock;
+  // persistent
+  blocksMap: Map<BlockId, IBlock>;
+  // transient, f(page)
+  rootBlockId: BlockId;
+  // transient, f(mouse events)
+  activeParentId: BlockId;
+  activeParentIndex: HierarchyIndex;
   selectionRange: SelectionRange;
   isSelectionActive: boolean;
+  isSelectionDeep: boolean;
 }
 
 export interface IBlock {
-  index: HierarchyIndex; // ids of ancestors and self, descending from root
-  id: string;
-  humanText: string;
-  selected: boolean;
-  children: IBlock[];
+  // persistent
+  id: BlockId;
+  humanText: HumanText;
+  children: BlockId[];
+  // transient states, like selectedness and hierarchyIndex, are calculated from state during render
 }
 
+export type BlockId = string;
+export type HumanText = string;
 export type HierarchyIndex = number[];
 export type SelectionRange = {start: HierarchyIndex, end: HierarchyIndex};
